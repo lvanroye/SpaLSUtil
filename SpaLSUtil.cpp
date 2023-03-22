e68aa279d8cad83e8870deca3709635317ce4881
landerv <lander.vanroye@kuleuven.be>
2023-03-22 15:02:57 +0100

    cleanup

--- SpaLSUtil.cpp
+++ SpaLSUtil.cpp
@@ -10,22 +10,7 @@ int main()
     auto expr1 = (a+b);
     auto expr = expr1*x + Const(5)*y + expr1*a*y + a*b*x;
     cout << "expr:" << endl;
-    cout << expr << endl;
-    // print the coefficients
-    cout << "coefficients:" << endl;
     auto coefficients = GetCoefficients(expr, {x, y});
-    for(auto coefficient : coefficients)
-    {
-        cout << coefficient << endl;
-    }
-    // order depth first
-    cout << "order depth first:" << endl;
-    vector<Expression> ordered_expr;
-    OrderDepthFirstRecurse(expr, ordered_expr);
-    for(auto expr : ordered_expr)
-    {
-        cout << expr << endl;
-    }
     Function testf({a, b}, coefficients.at(0));
     cout << testf.Eval({1, 2}) << endl;
     return 0;
