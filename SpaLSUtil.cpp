816457c0677daa4eeb4fb0d9b72d2013f3f08fae
landerv <lander.vanroye@kuleuven.be>
2023-04-11 10:02:26 +0200

    improvements

--- SpaLSUtil.cpp
+++ SpaLSUtil.cpp
@@ -11,11 +11,17 @@ int main()
 
     auto expr = (a + b + 1) * (x + y) - 5 * x;
     auto terms = GetTerms(expr, SymVec({x, y}));
+    // terms
+    cout << "terms: "<< endl;
+    for (auto term : terms)
+        cout << term << endl;
     auto coeffs = GetCoefficients({expr}, {x, y});
     // coefficients
     cout << "coefficients: "<< endl;
-    for (auto coeff : coeffs.at(0))
-        cout << coeff << endl;
+    for (auto coeff : coeffs)
+        // cout << coeff.row << endl;
+        // cout << coeff.col << endl;
+        cout << coeff.value << endl;
     return 0;
     // linear_system.add_equation(-X.at(k+1) + A.at(K) * X.at(k) + B.at(k) * U.at(k), 0);
 };
